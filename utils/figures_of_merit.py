2a3dcf9506b3118141ed5b97aaf3846a8cf05b90
AlWa88 <alexander.waldejer@hotmail.com>
2025-06-29 21:26:58 +0200

    refactor and added evaluation of slew distortion which evaluates y (no slew) and y_slewed calculating an rms error value

--- utils/figures_of_merit.py
+++ utils/figures_of_merit.py
@@ -14,7 +14,9 @@ The curve-fitting-based method is recommended based on extensive testing.
 """
 
 import numpy as np
+from scipy.interpolate import interp1d
 import math
+from enum import Enum, auto
 
 from scipy import signal
 from scipy import integrate
@@ -24,8 +26,11 @@ from utils.welch_psd import welch_psd
 from utils.psd_measurements import find_psd_peak
 from utils.fit_sinusoid import fit_sinusoid, sin_p
 
+class SINAD_COMP(Enum):
+    FFT = auto()  # FFT based
+    CFIT = auto()  # curve fit
 
-def TS_SINAD(x, t, make_plot=False, plot_label=''):
+def TS_SINAD(x, t, y_fit=None, make_plot=False, plot_label=''):
     """
     Take a time-series for computation of the SINAD using a curve-fitting method.
     Use at least 5 periods of the fundamental carrier signal for a good estimate
@@ -33,20 +38,25 @@ def TS_SINAD(x, t, make_plot=False, plot_label=''):
     """
 
     p_opt = fit_sinusoid(t, x, 1)
-    print("p_opt: ", p_opt)  # fitted params.
+    # print("p_opt: ", p_opt)  # fitted params.
+    # p_opt[0] = 9.92156862745098
     x_fit = sin_p(t, *p_opt)
+
 
     if make_plot:
         plt.figure()
-        plt.plot(t, x, 'r--', label=plot_label)
-        plt.plot(t, x_fit, 'g--', label='fit: A=%5.3f, f=%5.3f, phi=%5.3f, C=%5.3f' % tuple(p_opt))
-
+        plt.plot(t, x, label=plot_label)
+        plt.plot(t, x_fit, '--', label='fit: A=%5.3f, f=%5.3f, phi=%5.3f, C=%5.3f' % tuple(p_opt))
+        plt.plot(t, x-x_fit, label='error: x-x_fit')
+        plt.title(plot_label)
         plt.xlabel('t')
         plt.ylabel('out')
         plt.legend()
         # plt.show()
 
     error = x - x_fit
+    error_rms = np.sqrt(np.mean(error**2))
+    print(f'cfit rms error: {error_rms*1000}mV')
 
     sine_amp = p_opt[0]
     power_c = sine_amp**2/2
@@ -155,6 +165,74 @@ def FFT_SINAD(x, Fs, make_plot=False, plot_label=''):
 
     return SINAD
 
+def eval_enob_sinad(ty, y, Fs, TRANSOFF, SINAD_COMP_SEL:SINAD_COMP, print_results=True, plot=False, descr='', y_ref=None):
+    match SINAD_COMP_SEL:
+        case SINAD_COMP.FFT:  # use FFT based method to detemine SINAD
+            SINAD = FFT_SINAD(y[TRANSOFF:-TRANSOFF], Fs, plot, descr)
+        case SINAD_COMP.CFIT:  # use time-series sine fitting based method to detemine SINAD
+            y = y.reshape(1, -1).squeeze()
+            SINAD = TS_SINAD(y[TRANSOFF:-TRANSOFF], ty[TRANSOFF:-TRANSOFF], y_ref, plot, descr)
+
+    ENOB = (SINAD - 1.76)/6.02
+
+    # Print FOM
+    if print_results:
+        print(f'ENOB: {str(ENOB)} SINAD: {str(SINAD)}')
+
+    return ENOB, SINAD
+
+def eval_slew_distortion(y, y_slewed, t, t_slewed, print_results=False, plot_results=False, title=''):
+     # Eval Sum and average
+    y_avg = np.mean(y, axis=0, keepdims=True)
+    y_slewed_avg = np.mean(y_slewed, axis=0, keepdims=True)
+
+    # Extrapolate data if not sampled equally to match data lengths
+    if len(t) < len(t_slewed):
+        interp_slew = interp1d(t_slewed, y_slewed_avg, kind='cubic', fill_value='extrapolate')
+        y_slewed_avg = interp_slew(t)
+
+    slew_error = y_slewed_avg - y_avg
+    slew_error_rms = np.sqrt(np.mean(slew_error**2)) 
+  
+    # Alternative to deduct y_slewed_avg from ideal (analog) wave.
+    # This will include all errors and not distincly slewing as above.
+    
+    if print_results:
+        print(f'SUM and AVERAGE error: {str(slew_error_rms)}')   
+
+    if plot_results:
+        plt.figure()
+        plt.plot(t, y_avg.squeeze(), '-o', label='y_avg')
+        plt.plot(t, y_slewed_avg.squeeze(), '-o', label='y_slewed_avg')
+        plt.plot(t, slew_error.squeeze(), '-o', label='y_error')
+        if title != '':
+            plt.title('Slew distortion ' + title)
+        else:
+            plt.title('Slew distortion')
+        plt.legend()
+        plt.grid()
+
+    return slew_error, slew_error_rms
+
+def eval_slew_rate(y, t, print_results=False, plot_results=False):
+    # Calculate all dvdt
+    dv = np.diff(y)
+    dt = np.diff(t)
+    dvdt = dv/dt
+    dvdt_us = dvdt/1e-6
+    dvdt_us_no_zero = dvdt_us[dvdt_us != 0]
+    closest_to_zero = dvdt_us_no_zero[np.argmin(np.abs(dvdt_us_no_zero))]
+
+    if print_results:
+        print(f'Slew rate worst case: {str(closest_to_zero)}')
+
+    # if plot_results:
+    #     plt.figure()
+    #     plt.plot(t, y_avg.squeeze(), label='y_avg')
+    #     plt.plot(t, y_slewed_avg.squeeze(), label='y_slewed_avg')
+    #     plt.plot(t, slew_error.squeeze(), label='y_error')
+    #     plt.title('Slew distortion')
+    #     plt.legend()
 
 def main():
     """
